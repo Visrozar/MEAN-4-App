2a3f640ef3f5ddf8de9deda34af4983f2f1cf2a9
Visrozar <elvisrozarioefr@gmail.com>
2017-10-31 12:49:13 +0530

    Backend registration anduser model in Mongo

--- config/database.js
+++ config/database.js
@@ -2,5 +2,6 @@ const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
     uri: 'mongodb://localhost:27017/virtual-biotech',
-    secret: crypto
+    secret: crypto,
+    db: 'virtual-biotech'
 }
