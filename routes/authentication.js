95daac2bd1ca585ca61ccd2aa0db69f590045b40
Visrozar <elvisrozarioefr@gmail.com>
2017-12-05 08:57:52 +0530

    Added role to registration

--- routes/authentication.js
+++ routes/authentication.js
@@ -17,99 +17,110 @@ module.exports = (router) => {
         if (!req.body.password) {
           res.json({ success: false, message: 'You must provide a password' }); // Return error
         } else {
-          // Create new user object and apply user input
-          let user = new User({
-            email: req.body.email.toLowerCase(),
-            username: req.body.username.toLowerCase(),
-            password: req.body.password
-          });
-          // Save user to database
-          user.save((err) => {
-            // Check if error occured
-            if (err) {
-              // Check if error is an error indicating duplicate account
-              if (err.code === 11000) {
-                res.json({ success: false, message: 'Username or e-mail already exists' }); // Return error
-              } else {
-                // Check if error is a validation rror
-                if (err.errors) {
-                  // Check if validation error is in the email field
-                  if (err.errors.email) {
-                    res.json({ success: false, message: err.errors.email.message }); // Return error
-                  } else {
-                    // Check if validation error is in the username field
-                    if (err.errors.username) {
-                      res.json({ success: false, message: err.errors.username.message }); // Return error
+          // Check if password was provided
+          if (!req.body.role) {
+            res.json({ success: false, message: 'You must provide a role' }); // Return error
+          } else {
+            // Create new user object and apply user input
+            let user = new User({
+              email: req.body.email.toLowerCase(),
+              username: req.body.username.toLowerCase(),
+              password: req.body.password,
+              role: req.body.role.toLowerCase()
+            });
+            // Save user to database
+            user.save((err) => {
+              // Check if error occured
+              if (err) {
+                // Check if error is an error indicating duplicate account
+                if (err.code === 11000) {
+                  res.json({ success: false, message: 'Username or e-mail already exists' }); // Return error
+                } else {
+                  // Check if error is a validation rror
+                  if (err.errors) {
+                    // Check if validation error is in the email field
+                    if (err.errors.email) {
+                      res.json({ success: false, message: err.errors.email.message }); // Return error
                     } else {
-                      // Check if validation error is in the password field
-                      if (err.errors.password) {
-                        res.json({ success: false, message: err.errors.password.message }); // Return error
+                      // Check if validation error is in the username field
+                      if (err.errors.username) {
+                        res.json({ success: false, message: err.errors.username.message }); // Return error
                       } else {
-                        res.json({ success: false, message: err }); // Return any other error not already covered
+                        // Check if validation error is in the password field
+                        if (err.errors.password) {
+                          res.json({ success: false, message: err.errors.password.message }); // Return error
+                        } else {
+                          // Check if validation error is in the role field
+                          if (err.errors.role) {
+                            res.json({ success: false, message: err.errors.role.message }); // Return error
+                          } else {
+                            res.json({ success: false, message: err }); // Return any other error not already covered
+                          }
+                        }
                       }
                     }
+                  } else {
+                    res.json({ success: false, message: 'Could not save user. Error: ', err }); // Return error if not related to validation
                   }
-                } else {
-                  res.json({ success: false, message: 'Could not save user. Error: ', err }); // Return error if not related to validation
                 }
+              } else {
+                res.json({ success: true, message: 'Acount registered!' }); // Return success
               }
-            } else {
-              res.json({ success: true, message: 'Acount registered!' }); // Return success
-            }
-          });
+            });
+          }
         }
       }
     }
   });
-  
-    /* ============================================================
-       Route to check if user's email is available for registration
-    ============================================================ */
-    router.get('/checkEmail/:email', (req, res) => {
-      // Check if email was provided in paramaters
-      if (!req.params.email) {
-        res.json({ success: false, message: 'E-mail was not provided' }); // Return error
-      } else {
-        // Search for user's e-mail in database;
-        User.findOne({ email: req.params.email }, (err, user) => {
-          if (err) {
-            res.json({ success: false, message: err }); // Return connection error
+
+  /* ============================================================
+     Route to check if user's email is available for registration
+  ============================================================ */
+  router.get('/checkEmail/:email', (req, res) => {
+    // Check if email was provided in paramaters
+    if (!req.params.email) {
+      res.json({ success: false, message: 'E-mail was not provided' }); // Return error
+    } else {
+      // Search for user's e-mail in database;
+      User.findOne({ email: req.params.email }, (err, user) => {
+        if (err) {
+          res.json({ success: false, message: err }); // Return connection error
+        } else {
+          // Check if user's e-mail is taken
+          if (user) {
+            res.json({ success: false, message: 'E-mail is already taken' }); // Return as taken e-mail
           } else {
-            // Check if user's e-mail is taken
-            if (user) {
-              res.json({ success: false, message: 'E-mail is already taken' }); // Return as taken e-mail
-            } else {
-              res.json({ success: true, message: 'E-mail is available' }); // Return as available e-mail
-            }
+            res.json({ success: true, message: 'E-mail is available' }); // Return as available e-mail
           }
-        });
-      }
-    });
-  
-    /* ===============================================================
-       Route to check if user's username is available for registration
-    =============================================================== */
-    router.get('/checkUsername/:username', (req, res) => {
-      // Check if username was provided in paramaters
-      if (!req.params.username) {
-        res.json({ success: false, message: 'Username was not provided' }); // Return error
-      } else {
-        // Look for username in database
-        User.findOne({ username: req.params.username }, (err, user) => {
-          // Check if connection error was found
-          if (err) {
-            res.json({ success: false, message: err }); // Return connection error
+        }
+      });
+    }
+  });
+
+  /* ===============================================================
+     Route to check if user's username is available for registration
+  =============================================================== */
+  router.get('/checkUsername/:username', (req, res) => {
+    // Check if username was provided in paramaters
+    if (!req.params.username) {
+      res.json({ success: false, message: 'Username was not provided' }); // Return error
+    } else {
+      // Look for username in database
+      User.findOne({ username: req.params.username }, (err, user) => {
+        // Check if connection error was found
+        if (err) {
+          res.json({ success: false, message: err }); // Return connection error
+        } else {
+          // Check if user's username was found
+          if (user) {
+            res.json({ success: false, message: 'Username is already taken' }); // Return as taken username
           } else {
-            // Check if user's username was found
-            if (user) {
-              res.json({ success: false, message: 'Username is already taken' }); // Return as taken username
-            } else {
-              res.json({ success: true, message: 'Username is available' }); // Return as vailable username
-            }
+            res.json({ success: true, message: 'Username is available' }); // Return as vailable username
           }
-        });
-      }
-    });
-  
-    return router; // Return router object to main index.js
-  }
+        }
+      });
+    }
+  });
+
+  return router; // Return router object to main index.js
+}
