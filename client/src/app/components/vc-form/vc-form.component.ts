3aeba30fc7f51f4d251e8fa9071f84f7098d8c76
isaac-nadar <isaac.v.nadar@gmail.com>
2017-12-30 06:59:31 +0530

    added vc-from component

---
+++ client/src/app/components/vc-form/vc-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vc-form',
+  templateUrl: './vc-form.component.html',
+  styleUrls: ['./vc-form.component.scss']
+})
+export class VcFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
