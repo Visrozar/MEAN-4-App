fe39d4aac1dff01d4e97907daf993279185d63af
Visrozar <elvisrozarioefr@gmail.com>
2018-02-14 18:23:39 +0530

    Deployment Changes

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
-    uri: 'mongodb://localhost:27017/virtual-biotech',
+    uri: 'mongodb://isaac:pass123@ds161136.mlab.com:61136/angular-4-app',
     secret: crypto,
-    db: 'virtual-biotech'
+    db: 'angular-4-app'
 }
--- index.js
+++ index.js
@@ -10,24 +10,30 @@ const filters = require('./routes/filters')(router);
 const vcs = require('./routes/vcs')(router);
 const bodyParser = require('body-parser');
 
+//For Heroku deployment		 +//mongoose DB connection
+const port = process.env.PORT || 8000;
+const dev = '/client/dist';
+const prod = '/public';
+const env = prod;
+
 //mongoose DB connection
 mongoose.connect(config.uri, { useMongoClient: true }, (err) => {
-    if(err){
+    if (err) {
         console.log('Could not connect to database: ' + err);
     } else {
-        console.log('Connected to database');
+        console.log('Connected to ' + config.db); // Return success message
     }
 });
 mongoose.Promise = global.Promise;
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 //parse application/json
 app.use(bodyParser.json());
 
 // Static directory for frontend
-app.use(express.static(__dirname + '/client/dist/'))
+app.use(express.static(__dirname + env))
 //Parent autentication route, within this route lies registration and login
 app.use('/authentication', authentication);
 app.use('/projects', projects);
@@ -35,9 +41,13 @@ app.use('/filters', filters);
 app.use('/vcs', vcs);
 // Connect server to Angular 4 index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+    res.sendFile(path.join(__dirname + env + '/index.html'));
 });
-  
-app.listen(8000, () => {
-    console.log('Listening to port 8000');
+
+app.get('**', (req, res) => {
+    res.sendFile(path.join(__dirname + env + '/index.html'));
+});
+
+app.listen(port, () => {
+    console.log('Listening to port ' + port);
 });
